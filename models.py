6d57689436fa67b6c35e3382cae726dde558fda1
suruthimohan22 <148845566+suruthimohan22@users.noreply.github.com>
2025-02-08 13:28:29 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Float, Text, ForeignKey
+from .database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    brand = Column(String)
+    price = Column(Float)
+    category = Column(String)
+    description = Column(Text)
+    supplier_id = Column(Integer, ForeignKey("suppliers.id"))
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    contact_info = Column(Text)
+    product_categories = Column(Text)
